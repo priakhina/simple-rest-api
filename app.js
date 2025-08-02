a888b610de6def7875edd2479893acdef1ef6253
Kseniia Priakhina <kseniia.priakhina@gmail.com>
2025-08-01 20:59:26 -0600

    Add an API documentation

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const bodyParser = require('body-parser');
+const unknownEndpoint = require('./middleware/unknownEndpoint');
 const errorHandler = require('./middleware/errorHandler');
-
 const usersRouter = require('./routes/users');
 const incomeRouter = require('./routes/income');
 const expensesRouter = require('./routes/expenses');
 
+const app = express();
+
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-});
+// express.static is a built-in middleware from Express that allows to serve static content
+// (http://expressjs.com/en/starter/static-files.html)
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 app.use('/income', incomeRouter);
 app.use('/expenses', expensesRouter);
 
+app.use(unknownEndpoint);
 app.use(errorHandler);
 
 module.exports = app;
