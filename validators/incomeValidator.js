d8eef498dc03d280742bd3e54b78389eb12a0175
Kseniia Priakhina <kseniia.priakhina@gmail.com>
2025-08-01 19:04:22 -0600

    Update validation of income data to make all fields required

--- validators/incomeValidator.js
+++ validators/incomeValidator.js
@@ -2,10 +2,10 @@ const Joi = require('joi');
 
 const incomeSchema = Joi.object({
   wages: Joi.number().min(0).required(),
-  secondaryIncome: Joi.number().min(0).default(0),
-  interest: Joi.number().min(0).default(0),
-  supportPayment: Joi.number().min(0).default(0),
-  other: Joi.number().min(0).default(0),
+  secondaryIncome: Joi.number().min(0).required(),
+  interest: Joi.number().min(0).required(),
+  supportPayment: Joi.number().min(0).required(),
+  other: Joi.number().min(0).required(),
 });
 
 module.exports = { incomeSchema };
